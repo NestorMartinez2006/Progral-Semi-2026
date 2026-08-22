4fc4d17be58d5085284d2f46337729b14e3e5dd2
NestorMartinez2006 <ezequielalfa503@gmail.com>
2026-08-21 18:07:02 -0600

    Add files via upload
    desarrollo de la media aritmetica

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace miPrimeaAplicacion {
+    partial class Form1 {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.txtSerie = new System.Windows.Forms.TextBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.ltsValores = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtSerie
+            // 
+            this.txtSerie.Location = new System.Drawing.Point(277, 34);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(100, 22);
+            this.txtSerie.TabIndex = 0;
+          
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(277, 302);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.TabIndex = 1;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            // 
+            // ltsValores
+            // 
+            this.ltsValores.FormattingEnabled = true;
+            this.ltsValores.ItemHeight = 16;
+            this.ltsValores.Location = new System.Drawing.Point(277, 164);
+            this.ltsValores.Name = "ltsValores";
+            this.ltsValores.Size = new System.Drawing.Size(120, 84);
+            this.ltsValores.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 409);
+            this.Controls.Add(this.ltsValores);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.txtSerie);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Conversor de Longitud";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSerie;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.ListBox ltsValores;
+    }
+}
+
