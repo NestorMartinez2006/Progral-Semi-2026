1e97ba0101d87220e83ebe0f25f4bcf3c084fcd0
NestorMartinez2006 <ezequielalfa503@gmail.com>
2026-08-21 18:31:59 -0600

    calculo de la desviacion tipica

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -26,42 +26,55 @@
             this.txtSerie = new System.Windows.Forms.TextBox();
             this.btnProcesar = new System.Windows.Forms.Button();
             this.ltsValores = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtSerie
             // 
             this.txtSerie.Location = new System.Drawing.Point(277, 34);
+            this.txtSerie.Multiline = true;
             this.txtSerie.Name = "txtSerie";
             this.txtSerie.Size = new System.Drawing.Size(100, 22);
             this.txtSerie.TabIndex = 0;
-          
+            // 
             // btnProcesar
             // 
             this.btnProcesar.Location = new System.Drawing.Point(277, 302);
             this.btnProcesar.Name = "btnProcesar";
             this.btnProcesar.Size = new System.Drawing.Size(75, 23);
             this.btnProcesar.TabIndex = 1;
-            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.Text = "calcular estadistica";
             this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
             // 
             // ltsValores
             // 
             this.ltsValores.FormattingEnabled = true;
             this.ltsValores.ItemHeight = 16;
-            this.ltsValores.Location = new System.Drawing.Point(277, 164);
+            this.ltsValores.Location = new System.Drawing.Point(177, 100);
             this.ltsValores.Name = "ltsValores";
-            this.ltsValores.Size = new System.Drawing.Size(120, 84);
+            this.ltsValores.Size = new System.Drawing.Size(368, 148);
             this.ltsValores.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ingrese los numeros;";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(654, 409);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ltsValores);
             this.Controls.Add(this.btnProcesar);
             this.Controls.Add(this.txtSerie);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Conversor de Longitud";
             this.ResumeLayout(false);
@@ -74,6 +87,7 @@
         private System.Windows.Forms.TextBox txtSerie;
         private System.Windows.Forms.Button btnProcesar;
         private System.Windows.Forms.ListBox ltsValores;
+        private System.Windows.Forms.Label label1;
     }
 }
 
