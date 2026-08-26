e34d18bc858b060bcb31ab2b8b70cb1c5da32a1d
Ezequiel Martínez <ezequielalfa503@gmail.com>
2026-08-26 09:46:17 -0600

    creacion de el sistema de clinica

--- miPrimeaAplicacion/miPrimeaAplicacion/Form1.Designer.cs
+++ miPrimeaAplicacion/miPrimeaAplicacion/Form1.Designer.cs
@@ -26,42 +26,66 @@
             this.txtSerie = new System.Windows.Forms.TextBox();
             this.btnProcesar = new System.Windows.Forms.Button();
             this.ltsValores = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtSerie
             // 
-            this.txtSerie.Location = new System.Drawing.Point(277, 34);
+            this.txtSerie.Location = new System.Drawing.Point(169, 34);
             this.txtSerie.Name = "txtSerie";
-            this.txtSerie.Size = new System.Drawing.Size(100, 22);
+            this.txtSerie.Size = new System.Drawing.Size(410, 22);
             this.txtSerie.TabIndex = 0;
-          
+            // 
             // btnProcesar
             // 
-            this.btnProcesar.Location = new System.Drawing.Point(277, 302);
+            this.btnProcesar.Location = new System.Drawing.Point(459, 160);
             this.btnProcesar.Name = "btnProcesar";
-            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.Size = new System.Drawing.Size(120, 93);
             this.btnProcesar.TabIndex = 1;
             this.btnProcesar.Text = "Procesar";
             this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
             // 
             // ltsValores
             // 
             this.ltsValores.FormattingEnabled = true;
             this.ltsValores.ItemHeight = 16;
-            this.ltsValores.Location = new System.Drawing.Point(277, 164);
+            this.ltsValores.Location = new System.Drawing.Point(113, 164);
             this.ltsValores.Name = "ltsValores";
-            this.ltsValores.Size = new System.Drawing.Size(120, 84);
+            this.ltsValores.Size = new System.Drawing.Size(283, 180);
             this.ltsValores.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Serie:";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(459, 277);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(120, 89);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(654, 409);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ltsValores);
             this.Controls.Add(this.btnProcesar);
             this.Controls.Add(this.txtSerie);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Conversor de Longitud";
             this.ResumeLayout(false);
@@ -74,6 +98,8 @@
         private System.Windows.Forms.TextBox txtSerie;
         private System.Windows.Forms.Button btnProcesar;
         private System.Windows.Forms.ListBox ltsValores;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLimpiar;
     }
 }
 
--- miPrimeaAplicacion/miPrimeaAplicacion/Form1.cs
+++ miPrimeaAplicacion/miPrimeaAplicacion/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace miPrimeaAplicacion
@@ -17,17 +12,59 @@ namespace miPrimeaAplicacion
             InitializeComponent();
         }
 
-        private void btnProcesar_Click(object sender, EventArgs e)
+        private double media(double[] serie)
         {
-            String[] serie = txtSerie.Text.Split(',');
-            double[] miSerie = serie.Select(n => double.Parse(n)).ToArray();
-            double suma = 0;
+            return serie.Average();
+        }
+
+        private double desviacionTipica(double[] serie, double media)
+        {
+            return Math.Sqrt(serie.Average(n => Math.Pow(n - media, 2)));
+        }
+
+        private double armonica(double[] serie)
+        {
+            return serie.Length / serie.Sum(x => 1 / x);
+        }
+
+       
+        private double varianza(double[] serie, double media)
+        {
+            double sumaCuadrados = 0;
             for (int i = 0; i < serie.Length; i++)
             {
-                suma += miSerie[i];
+                sumaCuadrados += Math.Pow(serie[i], 2);
             }
-            double media = suma / miSerie.Length;
-            ltsValores.Items.Add("La media es: " + media);
+            double varianza = (sumaCuadrados / serie.Length) - Math.Pow(media, 2);
+            return varianza;
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+
+            if (string.IsNullOrWhiteSpace(txtSerie.Text)) return;
+
+            double[] miSerie = txtSerie.Text.Split(',')
+                                           .Select(n => double.Parse(n.Trim()))
+                                           .ToArray();
+
+            double m = media(miSerie);
+
+            ltsValores.Items.Add("La media es: " + m);
+            ltsValores.Items.Add("La desviacion tipica: " + desviacionTipica(miSerie, m));
+            ltsValores.Items.Add("La media armonica: " + armonica(miSerie));
+            ltsValores.Items.Add("La desviacion estandar es: " + varianza(miSerie, m));
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+
+        private void limpiar()
+        {
+            ltsValores.Items.Clear();
         }
     }
 }
